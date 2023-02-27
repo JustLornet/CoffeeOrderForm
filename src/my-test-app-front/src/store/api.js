da9c8a5ff96047e0d2ddf8d1e538ff3c85655a9e
DESKTOP-7HDQAKF\Влад <l0rnet.dv@gmail.com>
2023-02-28 00:20:14 +0700

    Поправлены настройки для корс сос стороны фронта

--- src/my-test-app-front/src/store/api.js
+++ src/my-test-app-front/src/store/api.js
@@ -3,9 +3,29 @@ import axios from "axios";
 export const BASE_BACK_PATH = "https://localhost:7133";
 
 export const get = path => {
-	return axios.get(`${BASE_BACK_PATH}${path}`);
+	const config = {
+        method: "GET",
+        credentials: "include",
+        mode: 'cors',
+        headers: new Headers({
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          }),
+    }
+
+	return axios.get(`${BASE_BACK_PATH}${path}`, config);
 };
 
 export const post = (path, json) => {
-	return axios.post(`${BASE_BACK_PATH}${path}`, json);
+	const config = {
+        method: "POST",
+        credentials: "include",
+        mode: 'cors',
+        headers: new Headers({
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          }),
+    }
+
+	return axios.post(`${BASE_BACK_PATH}${path}`, json, config);
 };
